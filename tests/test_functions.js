8414526ca83d2381b25ca9457adbf8cdb2831fb5
Todd Parsons <todd.e.parsons@googlemail.com>
2023-12-16 14:06:40 +0000

    test: More informative test suite

--- tests/test_functions.js
+++ tests/test_functions.js
@@ -12,30 +12,38 @@ let cases = [
     ["+44 3456050247", "\"+\", Gloom, Lileep, Poliwag, Dewott, Parasect"],  // Nintendo Europe
     ["+1 8002553700", "\"+\", Bulbasaur, Necrozma, Torchic, Venusaur, Sylveon"],  // Nintendo America
     ["+81 756629600", "\"+\", Magnemite, Shiinotic, Vullaby, Klang"], // Nintendo Japan
-    // one last value to clear the controls
-    ["", ""]
 ];
 // get ctrls
 const pokemon_ctrl = document.getElementById("pokemon_ctrl")
 const numbers_ctrl = document.getElementById("numbers_ctrl")
 // assume pass unless told otherwise
 let success = true;
+// log test start
+console.log("Starting missingno test...")
 // iterate through cases
 for (let this_case of cases) {
     // set num ctrl and update pokemon
     numbers_ctrl.value = this_case[0];
     numbers_ctrl.oninput();
-    // if values don't match...
-    if (pokemon_ctrl.value != this_case[1]) {
-        // raise error with info
-        console.error(
-            `Tried: ${this_case[0]}\nExpected: ${this_case[1]}\nGot: ${pokemon_ctrl.value}`
-        );
+    // prepare info message
+    let msg = `Tried: \t${this_case[0]}\nWanted:\t${this_case[1]}\nGot:   \t${pokemon_ctrl.value}`
+    // assess
+    if (pokemon_ctrl.value === this_case[1]) {
+        // print success
+        console.log("SUCCESS\n\n" + msg);
+    } else {
+        // print failure
+        console.error("FAILURE\n\n" + msg);
         // mark as failed
         success = false;
     }
 }
+// clear control
+numbers_ctrl.value = "";
+numbers_ctrl.oninput();
 // if no errors, print success
 if (success) {
     console.log("Missingno test completed successfully!")
+} else {
+    console.log("Missingno test completed with errors.")
 }
